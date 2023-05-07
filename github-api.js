85b5c91fc6b1442d3f5c6aa9afa9e981dbd2fdc6
antoprince001 <antoprince001@gmail.com>
2023-05-07 15:43:58 +0530

    Add graphql API request to fetch github data

---
+++ github-api.js
@@ -0,0 +1,85 @@
+const axios = require('axios');
+
+async function fetchRecentActivities(username, token) {
+    const fiveDaysAgo = new Date(new Date() - 5 * 24 * 60 * 60 * 1000).toISOString();
+    console.log(username+token)
+    const query = `
+    query { 
+      user(login: "antoprince001") { 
+        bio
+        contributionsCollection(from : "2023-04-30T08:55:09.282Z"){
+                totalCommitContributions
+                totalIssueContributions
+                totalPullRequestContributions	
+              totalPullRequestReviewContributions      	
+              
+        }
+        issues(last: 3){
+          nodes {
+                  title
+                  comments(last: 5) {
+                    edges {
+                      node {
+                        bodyText
+                      }
+                    }
+                  }
+                }
+        }
+        repositories(last: 3){
+          nodes {
+                  name
+                        languages(last: 3) {
+                          edges {
+                            node {
+                              name
+                            }
+                          }
+                        }
+                        description
+                      labels(last: 3) {
+                        edges {
+                          node {
+                            name
+                          }
+                        }
+                      }
+                        repositoryTopics(last: 3) {
+                          edges {
+                            node {
+                              topic {
+                                id
+                              }
+                            }
+                          }
+                        }
+                        stargazerCount
+                        
+                }
+        }
+        pullRequests(last: 3){
+          nodes{
+            title
+          }
+        }
+      }
+    }
+    `;
+        
+    const response = await axios({
+        url: 'https://api.github.com/graphql',
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        data: {
+          query: query
+        }
+      });
+    console.log(response.data)
+    return response.data;
+  }
+  const token = '';
+  const username = 'antoprince001'
+  fetchRecentActivities(username, token)
