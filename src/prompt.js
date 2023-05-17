658ce5b6a20342504134d5e480ecf972b9351996
antoprince001 <antoprince001@gmail.com>
2023-05-17 20:10:28 +0530

    Update prompt structure

--- src/prompt.js
+++ src/prompt.js
@@ -12,7 +12,8 @@ module.exports = async function generatePrompt(
        let githubUsage = await fetchGithubActivities(username,contributionPeriod)
        let prompt =  `
         Assume you are ${persona}. I am a github user who has
-        ${githubUsage}. Your response should not be negative and discouraging. 
+        ${githubUsage}. 
+        Your response should not be negative or discouraging. 
         ${action} relevant to my github contribution in less than ${outputLength} words 
         in a way that is ${tone}
        ` 
